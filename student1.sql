c082e2b8c60be8ced340963e7cb4e349c8bf7d1e
Sharez78 <190031510@kluniversity.in>
2021-01-22 16:52:16 +0530

    Update student1.sql

--- student1.sql
+++ student1.sql
@@ -1,2 +1,6 @@
 use student;
 create table student( reg_no int not null, name varchar(25) not null, email varchar(35) not null);
+INSERT INTO student (reg_no, name, email)
+VALUES ('190030592', 'varu', 'varu@gmail.com');
+INSERT INTO student (reg_no, name, email)
+VALUES ('190031071', 'Charlie', 'charlie@gmail.com');
